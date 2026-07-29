2e3ef95038934258010619dfa598af0482ea7cf3
Tim <tim8912097887@gmail.com>
2026-07-29 16:15:24 +0800

    feat(vote-service): add server and centralize shutdown

---
+++ services/vote_service/cmd/api/v1.go
@@ -0,0 +1,88 @@
+package api
+
+import (
+	"context"
+	"errors"
+	"log/slog"
+	"net/http"
+	"time"
+
+	"github.com/gofiber/fiber/v3"
+	"github.com/gofiber/fiber/v3/middleware/adaptor"
+	"github.com/tim8912097887-sys/Poll-Maker/services/vote_service/internal/shared/configs"
+	"github.com/tim8912097887-sys/Poll-Maker/services/vote_service/internal/shared/shutdown"
+)
+
+type ApiConfig struct {
+	Logger *slog.Logger
+	EnvConfigs configs.Configs
+}
+
+type Api struct {
+	Config ApiConfig
+}
+
+func (a *Api) Mount() http.Handler {
+	app := fiber.New()
+	app.Get("/health",func (c fiber.Ctx)  {
+		c.Status(http.StatusOK).JSON(fiber.Map{"status": "ok"})
+	})
+
+	return adaptor.FiberApp(app)
+}
+
+func (a *Api) Run(ctx context.Context, h http.Handler, shutdownTimeout time.Duration,shutdownManager *shutdown.Manager) error {
+	server := &http.Server{
+		Addr:    a.Config.EnvConfigs.Api.Addr,
+		Handler: h,
+		ReadTimeout:       5 * time.Second,
+        ReadHeaderTimeout: 2 * time.Second,
+        WriteTimeout:      10 * time.Second,
+        IdleTimeout:       120 * time.Second,
+	}
+
+	// Channel to notify when the server is initialized failure
+	serverErrorCh := make(chan error, 1)
+	// Start the server with goroutine
+	go func() {
+		a.Config.Logger.Info("starting server",slog.String("address", a.Config.EnvConfigs.Api.Addr))
+		if err := server.ListenAndServe(); err != nil && err != http.ErrServerClosed {
+			a.Config.Logger.Error("failed to start server",slog.Any("error", err))
+			serverErrorCh <- err
+		}
+	}()
+
+	// Register the shutdown handler
+	shutdownManager.Register(a.Shutdown(server, shutdownTimeout))
+
+	select {
+		case <-ctx.Done():
+			a.Config.Logger.Info("shutting down the server",slog.String("reason", ctx.Err().Error()))
+		case err := <-serverErrorCh:
+			return err
+	}
+
+	// Start a graceful shutdown
+	shutdownManager.Shutdown(shutdownTimeout)
+
+	return nil
+
+}
+
+func (a *Api) Shutdown(server *http.Server, shutdownTimeout time.Duration) func(context.Context) error {
+	
+	return func(ctx context.Context) error {
+
+		if err := server.Shutdown(ctx); err != nil {
+			a.Config.Logger.Error("failed to shut down the server",slog.Any("error", err))
+			if closeErr := server.Close(); closeErr != nil {
+				a.Config.Logger.Error("failed to close the server",slog.Any("error", err))
+				return errors.Join(err,closeErr)
+			}
+			return err
+		}
+
+		a.Config.Logger.Info("server shut down gracefully")
+		return nil
+	}
+}
