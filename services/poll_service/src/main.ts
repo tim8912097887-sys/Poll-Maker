68ff45dc76ca19e43373c909922ec08e1cc3451d
Tim <tim8912097887@gmail.com>
2026-08-05 07:24:52 +0800

    feat(poll-service): add grpc server and test it

--- services/poll_service/src/main.ts
+++ services/poll_service/src/main.ts
@@ -1,11 +1,23 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { GlobalExceptionFilter } from './shared/filters/global.filter';
+import { MicroserviceOptions, Transport } from '@nestjs/microservices';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalFilters(new GlobalExceptionFilter());
   app.enableShutdownHooks();
+  app.connectMicroservice<MicroserviceOptions>({
+    transport: Transport.GRPC,
+    options: {
+      package: 'poll.v1',
+      protoPath: './proto/poll.proto',
+      url: '0.0.0.0:50051',
+    },
+  });
+
+  // Start both servers
+  await app.startAllMicroservices();
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
---
+++ services/poll_service/test/poll.grpc.e2e.spec.ts
@@ -0,0 +1,163 @@
+import { INestApplication } from '@nestjs/common';
+import {
+  ClientGrpc,
+  ClientsModule,
+  MicroserviceOptions,
+  Transport,
+} from '@nestjs/microservices';
+import { Test } from '@nestjs/testing';
+import { sql } from 'drizzle-orm';
+import { drizzle } from 'drizzle-orm/node-postgres';
+import { AppModule } from 'src/app.module';
+import { DbAsyncProvider } from 'src/infrastructure/persistence/db.provider';
+import {
+  ValidatePollRequest,
+  ValidatePollResponse,
+  ValidatePollResponse_ValidityReason,
+} from 'src/proto/proto/poll';
+import { afterAll, beforeAll, beforeEach, describe, expect, it } from 'vitest';
+import { seedPolls } from './helpers/seeding/polls';
+import { RedisClientType } from 'redis';
+import { CacheAsyncProvider } from 'src/infrastructure/cache/cache.provider';
+import { firstValueFrom, Observable } from 'rxjs';
+
+interface PollService {
+  validatePollForVoting(
+    request: ValidatePollRequest,
+  ): Observable<ValidatePollResponse>;
+}
+
+describe('Polls gRPC API', () => {
+  let app: INestApplication;
+  let db: ReturnType<typeof drizzle>;
+  let cache: RedisClientType;
+  let client: ClientGrpc;
+  let pollService: PollService;
+
+  beforeAll(async () => {
+    const grpcOptions = {
+      package: 'poll.v1',
+      protoPath: './proto/poll.proto',
+      url: '0.0.0.0:50051',
+    };
+    const moduleRef = await Test.createTestingModule({
+      imports: [
+        AppModule,
+        ClientsModule.register([
+          {
+            name: 'POLL_MICRO_TEST_CLIENT',
+            transport: Transport.GRPC,
+            options: grpcOptions,
+          },
+        ]),
+      ],
+    }).compile();
+
+    app = moduleRef.createNestApplication();
+
+    app.connectMicroservice<MicroserviceOptions>({
+      transport: Transport.GRPC,
+      options: grpcOptions,
+    });
+
+    // Start all microservices attached to the app
+    await app.startAllMicroservices();
+    await app.init();
+
+    // Obtain gRPC client instance
+    client = moduleRef.get<ClientGrpc>('POLL_MICRO_TEST_CLIENT');
+    pollService = client.getService<PollService>('PollService');
+    // Obtain DB and cache instance
+    db = app.get(DbAsyncProvider);
+    cache = app.get(CacheAsyncProvider);
+  });
+
+  beforeEach(async () => {
+    await db.execute(sql`
+      TRUNCATE TABLE
+          polls,
+          poll_options
+      RESTART IDENTITY CASCADE
+      `);
+
+    await cache.flushAll();
+  });
+
+  afterAll(async () => {
+    await app.close();
+  });
+
+  describe('validatePollForVoting', () => {
+    it('should return true if poll is existing and valid', async () => {
+      // Arrange
+      const poll = await seedPolls(db, cache);
+      const id = poll.find(
+        (p) =>
+          !p.poll.isPrivate &&
+          p.poll.startedAt < new Date() &&
+          p.poll.expiredAt > new Date(),
+      )?.poll.id as string;
+
+      // Act
+      const respond = await firstValueFrom(
+        pollService.validatePollForVoting({ pollId: id }),
+      );
+
+      // Assert
+      expect(respond.isValid).toBe(true);
+      expect(respond.reason).toBe(ValidatePollResponse_ValidityReason.OK);
+    });
+
+    it('should return false if poll is not existing', async () => {
+      // Arrange
+      const id = '00000000-0000-0000-0000-000000000000';
+
+      // Act
+      const respond = await firstValueFrom(
+        pollService.validatePollForVoting({ pollId: id }),
+      );
+
+      // Assert
+      expect(respond.isValid).toBe(false);
+      expect(respond.reason).toBe(
+        ValidatePollResponse_ValidityReason.POLL_NOT_FOUND,
+      );
+    });
+
+    it('should return false if poll is expired', async () => {
+      // Arrange
+      const poll = await seedPolls(db, cache);
+      const id = poll.find((p) => p.poll.expiredAt < new Date())?.poll
+        .id as string;
+
+      // Act
+      const respond = await firstValueFrom(
+        pollService.validatePollForVoting({ pollId: id }),
+      );
+
+      // Assert
+      expect(respond.isValid).toBe(false);
+      expect(respond.reason).toBe(
+        ValidatePollResponse_ValidityReason.POLL_EXPIRED,
+      );
+    });
+
+    it('should return false if poll is not started yet', async () => {
+      // Arrange
+      const poll = await seedPolls(db, cache);
+      const id = poll.find((p) => p.poll.startedAt > new Date())?.poll
+        .id as string;
+
+      // Act
+      const respond = await firstValueFrom(
+        pollService.validatePollForVoting({ pollId: id }),
+      );
+
+      // Assert
+      expect(respond.isValid).toBe(false);
+      expect(respond.reason).toBe(
+        ValidatePollResponse_ValidityReason.POLL_NOT_STARTED,
+      );
+    });
+  });
+});
